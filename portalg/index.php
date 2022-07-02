bf30a258ef2bd2ae7fe327c2656f2709fcc02cb0
Leonardo C. Junior <leonardo.cjr@hotmail.com>
2022-07-02 15:12:43 -0300

    Hotsite informativo para comunidade no discord.

---
+++ portalg/index.php
@@ -0,0 +1,354 @@
+<html>
+<?php
+ $_rand_ads = rand(1, 1);$_rand_virtus_ads = rand(1, 11);if ($_rand_ads == "1") {  if ($_rand_virtus_ads == "1"){  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 125px;">Diga: Olá Mundo!</div>';  } elseif ($_rand_virtus_ads == "2") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 310px;">Para todo Game Over, existe um Play Again.</div>';  } elseif ($_rand_virtus_ads == "3") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 398px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</div>';  } elseif ($_rand_virtus_ads == "4") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 125px;">Skripter é gente?</div>';  } elseif ($_rand_virtus_ads == "5") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 220px;">&lt;?php echo "Hello World" ?&gt;;</div>';  }  elseif ($_rand_virtus_ads == "6") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 240px;">O kibe está quentinho no forno...</div>';  }  elseif ($_rand_virtus_ads == "7") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 230px;">System.out.println("Olá gamer");</div>';  }  elseif ($_rand_virtus_ads == "8") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 185px;">O dia que eu sai de casa...</div>';  }    elseif ($_rand_virtus_ads == "9") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 295px;">Na União Soviética, Java programa VOCÊ</div>';  }    elseif ($_rand_virtus_ads == "12") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 310px;">Cuidado... Deus Rosset pode estar olhando!</div>';  }    elseif ($_rand_virtus_ads == "11") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 225px;">Em parceria com <a href=https://lostonyx.com.br/>LostOnyx</a>... ❤</div>';  }    elseif ($_rand_virtus_ads == "10") {  $adsti = '<div style="color:#828282;border-bottom: dashed 1px #353535;cursor:pointer;margin-left: auto;margin-right: auto;width: 285px;">Isso não foi kibe do discord! Ou, talvez...</div>';  }}?>
+
+<head><title>PortalGaming - O seu portal digital de jogos e atividades gamers!</title>
+	<meta charset="UTF-8" />
+    <meta property="og:title" content="Introdução ao nosso Discord! - PortalGaming"/>
+    <meta description="Introdução sobre o discord oficial do PortalGaming, visite-nos!" />
+    <meta property="og:site_name" content="PortalGaming" />
+    <meta name="viewport" content="width=1024px" />
+    <meta name="theme-color" content="#ff3030" />
+    <link rel="image_src" href='https://portalgaming.com.br/logo.png' />
+    <meta property="og:url" content="https://portalgaming.com.br" />
+    <meta property="og:image" content="https://portalgaming.com.br/logo.png"/>
+    <meta property="og:description" content="O seu portal digital preferido na internet! Discute livremente em nossa comunidade sobre jogos, programação, design e divirta-se acima de tudo!" />
+    <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous">
+    </script>
+    <link href="https://fonts.googleapis.com/css?family=Lemon|Lily+Script+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico" />
+   <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+   <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto" rel="stylesheet">
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <style>
+    body {text-align: center;padding: 50px;
+        background: #2C2F33 url(wp_bg.png);
+        margin-left:200px;
+        margin-right:200px;
+
+    }
+    #clickedleeow {cursor:pointer;}
+
+    .btn btn-outline-danger:hover,btn btn-outline-warning:hover,btn btn-outline-primary:hover {    -webkit-transition: background 2s;    transition: background 2s;}
+    .btn-outline-info {color:#7289DA !important;border-color: #7289DA !important;}
+    .btn-outline-info:hover {background-color:#7289DA !important;color: #fff !important;fill: #fff !important;}
+    .btn-outline-info a:hover svg {fill:#fff !important;}
+    .preload{    
+        position: fixed;    
+        z-index:99999;    
+        top:0; 
+        left:0;    
+        width:100%; 
+        height:100%;    
+        background-color:#2C2F33;      
+        background-position:50% 50%;    
+        background-repeat:no-repeat;
+    }
+    
+    #discord{
+        margin-top:300px;
+        width: 15%;
+        margin-bottom: -30px;
+        }
+
+    #title1 {
+        text-align: center;
+        font-family: 'Lemon', cursive;
+        font-size: 60pt;
+        color: #ff2626;
+        text-shadow:0px 0.05em 0px #bc1a1a;
+        margin-right: 400px;
+    }
+    #title2 {
+        text-align: center;
+        font-family: 'Lemon', cursive;
+        font-size: 60pt;
+        color: #fff;
+        text-shadow:0px 0.05em 0px #c9c9c9;
+        margin-left: 10px;
+        margin-top: -20px;
+    }
+    #subtitle {
+        text-align: center;
+        margin-top:10px;
+        font-size: 30pt;
+        color: #7289DA;
+        text-shadow:0px 0.05em 0px #4f609e;
+        font-family: 'Lily Script One', cursive;
+    }
+
+    -webkit-keyframes swinging{
+    0%{-webkit-transform: rotate(3deg);}
+    50%{-webkit-transform: rotate(-1deg)}
+    100%{-webkit-transform: rotate(3deg);}
+}
+ 
+    @keyframes swinging{
+    0%{transform: rotate(3deg);}
+    50%{transform: rotate(-1deg)}
+    100%{transform: rotate(3deg);}
+}
+ 
+    .swingimage{
+    -webkit-transform-origin: 50% 0;
+    transform-origin: 50% 0;
+    -webkit-animation: swinging 3.5s ease-in-out forwards infinite;
+    animation: swinging 3.5s ease-in-out forwards infinite;
+}
+    #container{
+    width:400px;
+    height:450px;
+    background:#2C2F33;
+    border:#2C2F33 dashed 2px;
+    position: relative;
+    display: inline;
+    margin-top:50px;
+    opacity: 0.8;
+    }
+    #botao {
+        border-radius:4px;
+        cursor: pointer;
+        font-family: 'Oswald', sans-serif;
+        font-size:20px;
+        display:inline;
+        margin-top:25px;
+        margin-left:15px;
+        margin-right:15px;
+        color:#fff;
+        transition:all .1s linear 0s;
+        padding:4px 12px 5px;
+        outline:0;
+    }
+    #botao:active {
+  background-color: #3e8e41;
+  box-shadow: 0 3px rgba(0, 0, 0, 0.1);;
+  transform: translateY(4px);
+}
+    #botao a {
+            text-decoration: none;        
+            color:none;
+    }
+    #botao a:hover {
+        color:none;
+    }
+    a {
+    text-decoration: none;
+    }
+    a:hover {
+        text-decoration: none;
+    }
+    </style>
+</head>
+<body onLoad="setTimeout(carregar, 1500);">
+    <span id='topo'></span>
+<div id="preload" class="preload"><video id="discord" autoplay="" loop="" class="ready-2UbIdM">
+    <source src="https://discordapp.com/assets/0bdc0497eb3a19e66f2b1e3d5741634c.webm">
+    <source src="https://discordapp.com/assets/ffac5bb3fb919ce8bf7137d79e9defc9.mp4" type="video/mp4">
+    <img src="https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png">
+</video>  <br>  <?php echo $adsti; ?></div>
+<script>
+function carregar() {
+    document.getElementById("preload").style.display="none";
+}
+</script>
+<h1><div id="title1">Portal</div></h1>
+<h1><div id="title2">Gaming</div></h1>
+<br>
+<div id="subtitle" class="swingimage">O seu portal digital de jogos e atividades gamers!</div>
+
+<a href="https://portalgaming.com.br" target="_blank"><button id="botao" style="background:#f93b3b;box-shadow:0 5px 0 #c62d2d;border:0;"><i class="fas fa-bullhorn"></i> Fórum</button></a>
+<a href="https://portalgaming.com.br/discord" target="_blank"><button id="botao" style="background:#7289DA;box-shadow:0 5px 0 #5a6cad;border:0;"><i class="fab fa-discord"></i> Discord</button></a>
+<a href="https://discordapp.com/api/oauth2/authorize?client_id=417042554450739200&redirect_uri=portal-bot.ga%2Fservers&response_type=code&scope=identify%20guilds%20guilds.join%20email" target="_blank"><button id="botao" style="background:#048def;box-shadow:0 5px 0 #0571bf;border:0;"><i class="far fa-arrow-alt-circle-right"></i> Entrar</button></a>
+<style>
+    #titulo {
+        font-family: 'Pacifico', cursive;
+        font-size:30px;
+        padding:5px;
+        text-align: center;
+        color:#fff;
+        text-shadow:0px 0.05em 0px #000;
+    }
+#ranks {
+    margin: auto;
+    margin-top:60px;
+    background: rgba(176, 170, 255, 0.3);
+    padding:30px;
+    border-radius: 20px;
+    width: 605px;
+    }
+#sla {
+    color:hsla(0,0%,100%,.7);
+    font-size:0.9375rem;
+    line-height:1.1em;
+    background-color: #2C2F33;
+    font-family:inherit;
+    text-align: left;
+    padding:10px;
+    border-radius: 20px;
+}
+#mention {
+    color:#7289da;
+    font-weight:500;
+    display: inline;
+    cursor:pointer;
+}
+</style>
+<div id="ranks">
+    <div id="titulo" style="margin-top:-30px;">Ranks</div>
+    <div id="sla">
+        <span id="mention" style="color: rgb(52, 152, 219); background-color: rgba(52, 152, 219, 0.1); border: none;">@👑 PortalMASTER [LV. 100]</span> - Adquirido no <b>level 100</b>.<br><br>
+        <span class="mention" style="color: rgb(255, 186, 186); background-color: rgba(255, 186, 186, 0.1); border: none;">@🎮 Gamer [LV. 80]</span> - Adquirido no <b>level 80</b>.<br><br>
+        <span class="mention" style="color: rgb(248, 186, 255); background-color: rgba(248, 186, 255, 0.1); border: none;">@🤞 Iludido [LV. 60]</span> - Adquirido no <b>level 60</b>.<br><br>
+        <span class="mention" style="color: rgb(186, 206, 255); background-color: rgba(186, 206, 255, 0.1); border: none;">@👨 Veterano [LV. 40]</span> - Adquirido no <b>level 40</b>.<br><br>
+        <span class="mention" style="color: rgb(186, 255, 250); background-color: rgba(186, 255, 250, 0.1); border: none;">@👨&zwj;💼 Noob [LV. 20]</span> - Adquirido no <b>level 20</b>.<br><br>
+        <span class="mention" style="color: rgb(224, 255, 186); background-color: rgba(224, 255, 186, 0.1); border: none;">@🤠 Cowboy [LV. 10]</span> - Adquirido no <b>level 10</b>.<br><br>
+        <span id="mention" style="color: rgb(150, 146, 146); background-color: rgba(150, 146, 146, 0.1); border: none;">@Homo Erectus [LV. 5]</span> - Adquirido no <b>level 5</b>.<br>
+    </div>
+    <div id="titulo">Comandos</div>
+    <div id="sla">
+<b>Comandos do <div id="mention">@PortalBot</div>!</b>
+<br><br>
+<b>!ajuda</b> - Veja todas informações de nosso discord!<br>
+<b>!notificar</b> - Seja notificado de nossos eventos e avisos informais!<br>
+<b>!sugerir &lt;título&gt; &lt;sugestão&gt;</b> - Sugerir coisas ao nosso servidor.<br>
+<b>!confessar &lt;confissão&gt;</b> - Falar sobre seus podres no ANONIMATO!<br>
+<b>!loja</b> - Ver todos itens de nossa loja.<br>
+<b>!moedas</b> - Ver quantas moedas você tem em nosso servidor!<br>
+<b>!pescar</b> - Jogar um jogo de pesca camarada e concorrer a shards.<br>
+<b>!minerar</b> - Jogar um jogo camarada e concorrer a shards.<br>
+<b>!tickets</b> - Ver quantos tickets de jogo você possui.<br>
+<b>!diario</b> - Pegar seus tickets do dia! (3)<br>
+<b>!eventos</b> - Vê todos eventos que acontecem em nosso discord!<br>
+
+    </div>
+    <div id="titulo">Canais</div>
+        <div id="sla">  
+<div id="mention">#🌌bem-vindo</div> - <i>Mensagens automáticos aos novos no servidor!</i><br />
+<div id="mention">#🔪punicoes</div> - <i>Punições do @Dyno#3861 !</i><br />
+<br /><br />
+<b>📣 | Importante</b><br />
+<div id="mention">#📌anúncios</div> - <i>Anúncios feitos pela nossa equipe de grande importância.</i><br />
+<div id="mention">#🐦tweets</div> - <i>Tweets automáticos por @_portalgaming.</i><br />
+<div id="mention">#📜changelog</div> - <i>Atualizações e mudanças no Discord.</i><br />
+<div id="mention">#📝regras</div> - <i>Regras do discord, siga-as para não ser punido.</i><br />
+<br /><br />
+<b>✌ | Início</b><br />
+<div id="mention">#💬bate-papo</div> - <i>Converse, interaja e tire dúvidas sobre quaisquer assuntos com outros membros.</i> <br />
+<div id="mention">#🎉eventos</div> - <i>Eventos automáticos pelo PortalBOT.</i><br />
+<div id="mention">#📣divulgacoes</div> - <i>Divulgações promovidas pela @👮 STAFF e membros com permissões compradas no discord.</i><br />
+<div id="mention">#📥apresente-se </div>- <i>Fale sobre você, ficamos felizes em te conhecer!</i><br />
+<br /><br />
+<b>😋 | Outros</b><br />
+<div id="mention">#🌟mitos</div> - <i>Mensagens escolhidas pelos membros com estrela!</i> <br />
+<div id="mention">#💡sugestoes</div> - <i>Sugestões pensadas pelos membros!</i><br />
+<div id="mention">#👀confessionario</div> - <i>Confissões SECRETAS feitas com o PortalBOT.</i><br />
+<div id="mention">#🌎mundo-das-tretas</div> - <i>Desconte suas raivas.</i> <br />
+<div id="mention">#📦content-nulled</div> - <i>Conteúdo nulled para os membros!</i> <br />
+<br /><br />
+<b>🤖 | Interação Robótica</b><br />
+<div id="mention">#💾bots</div> - <i>Bip, bip... Robôs...</i><br />
+<div id="mention">#💾pokecord-comandos</div> - <i>Pikachu... Eu escolho você!</i><br />
+<div id="mention">#🙈sem-regras</div> - <i>Feche os olhos crianças!</i><br />
+<b>🔊 Radio¹</b> - <i>Os batidão da <div id="mention">@PortalMorenitta (+help)</div></i><br />
+</div>
+</div>
+<br><br>
+</center><a href='#topo'>Voltar ao topo</a><center><br />
+<span style="color:#fff">Made with ❤️ by </span><a href="https://github.com/leonardocjr">Leonardo C. Junior</a>
+
+
+
+
+<SCRIPT LANGUAGE="JavaScript">
+<!-- Original:  Justin Arruda (JArruda@mindspring.com) -->
+
+<!-- This script and many more are available free online at -->
+<!-- The JavaScript Source!! http://javascript.internet.com -->
+
+<!-- Begin
+var step = 1;
+var delay = 30;
+var height = 0;
+var Hoffset = 0;
+var Woffset = 0;
+var yon = 0;
+var xon = 0;
+var pause = true;
+var interval;
+var name = navigator.appName;
+if(name == "Microsoft Internet Explorer") name = true;
+else name = false;
+var xPos = 20;
+if(name) var yPos = document.body.clientHeight;
+else var yPos = window.innerHeight;
+function changePos() {
+if(name) {
+width = document.body.clientWidth;
+height = document.body.clientHeight;
+Hoffset = img.offsetHeight;
+Woffset = img.offsetWidth;
+img.style.left = xPos + document.body.scrollLeft;
+img.style.top = yPos + document.body.scrollTop;
+}
+else {
+height = window.innerHeight;
+width = window.innerWidth;
+Hoffset = document.img.clip.height;
+Woffset = document.img.clip.width;
+document.img.pageY = yPos + window.pageYOffset;
+document.img.pageX = xPos + window.pageXOffset;
+}
+if (yon) {
+yPos = yPos + step;
+}
+else {
+yPos = yPos - step;
+}
+if (yPos < 0) {
+yon = 1;
+yPos = 0;
+}
+if (yPos >= (height - Hoffset)) {
+yon = 0;
+yPos = (height - Hoffset);
+}
+if (xon) {
+xPos = xPos + step;
+}
+else {
+xPos = xPos - step;
+}
+if (xPos < 0) {
+xon = 1;
+xPos = 0;
+}
+if (xPos >= (width - Woffset)) {
+xon = 0;
+xPos = (width - Woffset);
+   }
+}
+function start() {
+if(name) img.visibility = "visible";
+else document.img.visibility = "visible";
+interval = setInterval('changePos()',delay);
+}
+function pauseResume() {
+if(pause) {
+clearInterval(interval);
+pause = false;
+}
+else {
+interval = setInterval('changePos()',delay);
+pause = true;
+   }
+}
+start();
+//  End -->
+</script>
+</body></html>
+
+
